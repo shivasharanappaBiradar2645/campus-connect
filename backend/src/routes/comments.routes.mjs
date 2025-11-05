404b0bbb2f309a2501835b7d69427e0ce18a6d70
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2025-11-06 03:18:13 +0530

    added endpoint to fetch user data by id

--- backend/src/routes/comments.routes.mjs
+++ backend/src/routes/comments.routes.mjs
@@ -1,5 +1,11 @@
 import express from "express";
-import { createComment, getCommentsByThreadId, updateComment, deleteComment } from "../controllers/comments.controllers.mjs";
+import {
+    createComment,
+    getCommentsByThreadId,
+    updateComment,
+    deleteComment,
+    getAuthorData
+} from "../controllers/comments.controllers.mjs";
 import { authenticateToken } from "../middlewares/auth.mjs";
 
 const router = express.Router();
@@ -8,5 +14,5 @@ router.post("/", authenticateToken, createComment);
 
 router.put("/:id", authenticateToken, updateComment);
 router.delete("/:id", authenticateToken, deleteComment);
-
+router.get("/:authorId", getAuthorData)
 export default router;
