0d8a44833e9dcbf21237d557f24ad986fee65669
pranavdessaii <kishrgdesai@gmail.com>
2025-11-17 15:46:57 +0530

    .env (shiva)

--- backend/src/config.mjs
+++ backend/src/config.mjs
@@ -1,7 +1,13 @@
-export const JWT_SECRET_KEY = "plswork";
+import  dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+
+export const JWT_SECRET_KEY = process.env.JWT_KEY;
   
-export const DATABASE_URL="postgresql://postgres.rzhofmeljzgphudrsqub:k6wmUdK5yGFif4DQ@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+export const DATABASE_URL= process.env.DB_URL
+export const SUPABASE_URL = process.env.SUPABASE_URL
 
-export const SUPABASE_URL = 'https://rzhofmeljzgphudrsqub.supabase.co'
+export const SUPABASE_API_KEY = process.env.SUPABASE_KEY
 
-export const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aG9mbWVsanpncGh1ZHJzcXViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDg4NjIzNCwiZXhwIjoyMDc2NDYyMjM0fQ._Mf8F2O7xx2bwhQ9JQAzAOmO9mpO6iKKIDw2KLZZ0AI'
+console.log(JWT_SECRET_KEY)
